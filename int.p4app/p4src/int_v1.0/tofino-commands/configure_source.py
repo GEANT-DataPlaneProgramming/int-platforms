ec78321de207bfc4405f5543d94c26ef28ed7c38
Damu Ding <dingdamu@gmail.com>
2021-06-23 15:46:44 +0200

    fix a bug in configure_source.py

--- int.p4app/p4src/int_v1.0/tofino-commands/configure_source.py
+++ int.p4app/p4src/int_v1.0/tofino-commands/configure_source.py
@@ -2,10 +2,10 @@ p4 = bfrt.int.pipe.Ingress.Int_source.tb_int_source
 def setUp():
     global p4
     from ipaddress import ip_address
-    p4.add_with_configure_source(srcaddr=ip_address("10.0.1.1"),
-                                                    srcaddr_mask=0xFFFFFFFF,
-                                                    dstaddr=ip_address("10.0.2.2"),
-                                                    dstaddr_mask=0xFFFFFFFF,
+    p4.add_with_configure_source(srcAddr=ip_address("10.0.1.1"),
+                                                    srcAddr_mask=0xFFFFFFFF,
+                                                    dstAddr=ip_address("10.0.2.2"),
+                                                    dstAddr_mask=0xFFFFFFFF,
                                                     l4_src=0x11FF,
                                                     l4_src_mask=0x0000,
                                                     l4_dst=0x22FF,
@@ -14,10 +14,10 @@ def setUp():
                                                     hop_metadata_len = 10,
                                                     ins_cnt = 8,
                                                     ins_mask = 0xFF)
-    p4.add_with_configure_source(srcaddr=ip_address("10.0.3.3"),
-                                                    srcaddr_mask=0xFFFFFFFF,
-                                                    dstaddr=ip_address("10.0.4.4"),
-                                                    dstaddr_mask=0xFFFFFFFF,
+    p4.add_with_configure_source(srcAddr=ip_address("10.0.3.3"),
+                                                    srcAddr_mask=0xFFFFFFFF,
+                                                    dstAddr=ip_address("10.0.4.4"),
+                                                    dstAddr_mask=0xFFFFFFFF,
                                                     l4_src=0x11FF,
                                                     l4_src_mask=0x0000,
                                                     l4_dst=0x4268,
@@ -28,10 +28,10 @@ def setUp():
                                                     ins_mask = 0xCC)
     p4.dump()
     # modify an existing entry
-    p4.mod_with_configure_source(srcaddr=ip_address("10.0.3.3"),
-                                                    srcaddr_mask=0xFFFFFFFF,
-                                                    dstaddr=ip_address("10.0.5.5"),
-                                                    dstaddr_mask=0xFFFFFFFF,
+    p4.mod_with_configure_source(srcAddr=ip_address("10.0.3.3"),
+                                                    srcAddr_mask=0xFFFFFFFF,
+                                                    dstAddr=ip_address("10.0.5.5"),
+                                                    dstAddr_mask=0xFFFFFFFF,
                                                     l4_src=0x11FF,
                                                     l4_src_mask=0x0000,
                                                     l4_dst=0x4268,
