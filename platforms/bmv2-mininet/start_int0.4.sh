93d39e4c12ac1559811461542c44c5cf05e94a03
damianp <damianp@man.poznan.pl>
2021-09-16 16:19:11 +0200

    Restructuring done

--- platforms/bmv2-mininet/start_int0.4.sh
+++ platforms/bmv2-mininet/start_int0.4.sh
@@ -1,2 +1,5 @@
+cp ../../p4src int.p4app/ -r
 sudo cp p4app /usr/local/bin
 sudo p4app run int.p4app --manifest int_v0.4.json
+rm int.p4app/p4src -rf
+
--- platforms/bmv2-mininet/start_int1.0.sh
+++ platforms/bmv2-mininet/start_int1.0.sh
@@ -1,2 +1,4 @@
+cp ../../p4src int.p4app/ -r
 sudo cp p4app /usr/local/bin
 sudo p4app run int.p4app --manifest int_v1.0.json
+rm int.p4app/p4src -rf
